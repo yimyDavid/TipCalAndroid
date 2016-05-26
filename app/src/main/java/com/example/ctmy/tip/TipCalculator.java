882ee6604964c084decfe45f51cb3e306a3e9199
Yimy <yimysol@gmail.com>
2016-05-26 02:33:30 -0400

    Finished the UI

---
+++ app/src/main/java/com/example/ctmy/tip/TipCalculator.java
@@ -0,0 +1,13 @@
+package com.example.ctmy.tip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator);
+    }
+}
